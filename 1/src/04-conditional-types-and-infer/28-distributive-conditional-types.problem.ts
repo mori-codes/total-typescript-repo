e9ac1eb45a91cb272725185bf19cabf03844e1c2
mori-codes <mori.codes.oficial@gmail.com>
2023-04-05 11:32:32 +0200

    Lesson 4: Conditional types

--- 1/src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ 1/src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -5,5 +5,5 @@ type Fruit = "apple" | "banana" | "orange";
 type AppleOrBanana = Fruit extends "apple" | "banana"
   ? "apple" | "banana"
   : never;
-
+// Mirar las soluciones, no se puede hacer sin un contexto genérico
 type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
